02e9124d6d64d00d86697d001851bc47926ff0e9
surajit das <smartsurajit2007@gmail.com>
2020-05-30 16:56:44 +0300

    initial set up

--- Molly/settings.py
+++ Molly/settings.py
@@ -77,11 +77,20 @@ WSGI_APPLICATION = 'Molly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
+        'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
+
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
