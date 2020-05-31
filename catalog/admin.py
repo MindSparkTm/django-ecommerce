86f354d5f297d8e2adb250b4309abaacb35cd39b
surajit das <smartsurajit2007@gmail.com>
2020-05-31 14:47:33 +0300

    modified admin in catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Product, ProductImage, Category
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'is_active',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'sku',)
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'get_product_name')
+
+    def get_product_name(self, obj):
+        return obj.name
+
+    get_product_name.short_description = 'Product'
